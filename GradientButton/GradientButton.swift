817450881f5c059315ecfab3e3b01b99a3379014
Jonathan Tang <jonathan.tang@student.manchester.ac.uk>
2018-04-10 21:54:01 +0100

    Initial Xcode project commit

---
+++ GradientButton/GradientButton.swift
@@ -0,0 +1,16 @@
+//
+//  GradientButton.swift
+//  GradientButton
+//
+//  Created by Jonathan Tang on 10/04/2018.
+//  Copyright © 2018 jtang0506. All rights reserved.
+//  Source: http://github.com/jTanG0506/GradientButton
+//
+
+import UIKit
+
+class GradientButton: UIButton {
+  
+  
+  
+}
