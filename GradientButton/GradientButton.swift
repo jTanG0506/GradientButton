30b4a988407abcbf321b9740ea4fa162e2721c61
Jonathan Tang <jonathan.tang@student.manchester.ac.uk>
2018-04-10 22:03:58 +0100

    Add basic horizontal two colour gradient

--- GradientButton/GradientButton.swift
+++ GradientButton/GradientButton.swift
@@ -9,8 +9,23 @@
 
 import UIKit
 
+@IBDesignable
 class GradientButton: UIButton {
   
+  @IBInspectable var startColour: UIColor?
+  @IBInspectable var endColour: UIColor?
   
+  override func layoutSubviews() {
+    super.layoutSubviews()
+    
+    if let startColour = startColour, let endColour = endColour {
+      let gradientLayer = CAGradientLayer()
+      gradientLayer.colors = [startColour.cgColor, endColour.cgColor]
+      gradientLayer.frame = self.bounds
+      gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.5)
+      gradientLayer.endPoint = CGPoint(x: 1.0, y: 0.5)
+      self.layer.insertSublayer(gradientLayer, at: 0)
+    }
+  }
   
 }
